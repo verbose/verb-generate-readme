2ac2f5c174a882ed63931c4d3b2f18ddc2aa0d03
jonschlinkert <jon.schlinkert@sellside.com>
2016-03-24 17:00:15 -0400

    adds bower and downloads badges

--- templates/badges.js
+++ templates/badges.js
@@ -1,8 +1,15 @@
 'use strict';
 
 module.exports = {
-  travis: '[![Build Status](https://img.shields.io/travis/{%= repository %}.svg)](https://travis-ci.org/{%= repository %})',
-  fury: '[![NPM version](https://img.shields.io/npm/v/{%= name %}.svg)](https://www.npmjs.com/package/{%= name %})',
-  npm: '[![NPM version](https://img.shields.io/npm/v/{%= name %}.svg)](https://www.npmjs.com/package/{%= name %})',
-  coveralls: '[![Coverage Status](https://img.shields.io/coveralls/{%= repository %}.svg)](https://coveralls.io/r/{%= repository %})'
+  bower: '[![Bower version](https://img.shields.io/bower/v/{%= name %}.svg?style=flat)]',
+  
+  travis: '[![Build Status](https://img.shields.io/travis/{%= repository %}.svg?style=flat)](https://travis-ci.org/{%= repository %})',
+
+  fury: '[![NPM version](https://img.shields.io/npm/v/{%= name %}.svg?style=flat)](https://www.npmjs.com/package/{%= name %})',
+
+  npm: '[![NPM version](https://img.shields.io/npm/v/{%= name %}.svg?style=flat)](https://www.npmjs.com/package/{%= name %})',
+
+  downloads: '[![NPM downloads](https://img.shields.io/npm/dm/{%= name %}.svg?style=flat)](https://npmjs.org/package/{%= name %})',
+
+  coveralls: '[![Coverage Status](https://img.shields.io/coveralls/{%= repository %}.svg?style=flat)](https://coveralls.io/r/{%= repository %})'
 };
