02bd7af56cf52e0522c78d7b958da71fcb14baa4
jonschlinkert <jon.schlinkert@sellside.com>
2016-12-29 01:53:02 -0500

    organize badges

--- templates/badges.js
+++ templates/badges.js
@@ -4,18 +4,20 @@ module.exports = {
   appveyor: '[![Windows Build Status](https://img.shields.io/appveyor/ci/{%= repository %}.svg?style=flat&label=AppVeyor)](https://ci.appveyor.com/project/{%= repository %})',
 
   bower: '[![Bower version](https://img.shields.io/bower/v/{%= name %}.svg?style=flat)]',
-  
-  travis: '[![Build Status](https://img.shields.io/travis/{%= repository %}.svg?style=flat)](https://travis-ci.org/{%= repository %})',
 
-  travisLinus: '[![Linux Build Status](https://img.shields.io/travis/{%= repository %}.svg?style=flat&label=Travis)](https://travis-ci.org/{%= repository %})',
+  coveralls: '[![Coverage Status](https://img.shields.io/coveralls/{%= repository %}.svg?style=flat)](https://coveralls.io/r/{%= repository %})',
 
-  fury: '[![NPM version](https://img.shields.io/npm/v/{%= name %}.svg?style=flat)](https://www.npmjs.com/package/{%= name %})',
+  downloads: '[![NPM monthly downloads](https://img.shields.io/npm/dm/{%= name %}.svg?style=flat)](https://npmjs.org/package/{%= name %})',
+
+  'npm-downloads-monthly': '[![NPM monthly downloads](https://img.shields.io/npm/dm/{%= name %}.svg?style=flat)](https://npmjs.org/package/{%= name %})',
+
+  'npm-downloads-total': '[![NPM total downloads](https://img.shields.io/npm/dt/{%= name %}.svg?style=flat)](https://npmjs.org/package/{%= name %})',
 
   gitter: '[![Gitter](https://badges.gitter.im/join_chat.svg)](https://gitter.im/{%= repository %})',
 
   npm: '[![NPM version](https://img.shields.io/npm/v/{%= name %}.svg?style=flat)](https://www.npmjs.com/package/{%= name %})',
 
-  downloads: '[![NPM downloads](https://img.shields.io/npm/dm/{%= name %}.svg?style=flat)](https://npmjs.org/package/{%= name %})',
+  travis: '[![Build Status](https://img.shields.io/travis/{%= repository %}.svg?style=flat)](https://travis-ci.org/{%= repository %})',
 
-  coveralls: '[![Coverage Status](https://img.shields.io/coveralls/{%= repository %}.svg?style=flat)](https://coveralls.io/r/{%= repository %})'
+  travisLinus: '[![Linux Build Status](https://img.shields.io/travis/{%= repository %}.svg?style=flat&label=Travis)](https://travis-ci.org/{%= repository %})',
 };
