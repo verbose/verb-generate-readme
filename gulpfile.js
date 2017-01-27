437523d161c7da8dddc8aa0f5716513360e74f89
jonschlinkert <jon.schlinkert@sellside.com>
2017-01-27 14:19:01 -0500

    lint

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,7 @@ gulp.task('lint', function() {
 gulp.task('unused', function() {
   var keys = Object.keys(require('./lib/utils.js'));
   return gulp.src(['generator.js', 'lib/*.js'])
-    .pipe(unused({keys: keys}))
+    .pipe(unused({keys: keys}));
 });
 
 gulp.task('default', ['test', 'lint']);
